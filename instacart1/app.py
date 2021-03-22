4bf77483bcdaedb5dd9d34d5b41db4850f942340
Kavit Shah <34276425+kavitshah98@users.noreply.github.com>
2021-03-22 00:44:57 -0400

    Updated with some styling on the table

--- instacart1/app.py
+++ instacart1/app.py
@@ -9,61 +9,87 @@ import psycopg2
 
 app = Flask(__name__)
 
-db = pymysql.connect(host="database-2.cuxnpjy7qt1g.us-east-1.rds.amazonaws.com",
-                     user="admin",
-                     passwd="abcd1234",
-                     db="instacart_db")
 
-rsdb = psycopg2.connect(dbname= 'instacartredshift',
+
+'''rsdb = psycopg2.connect(dbname= 'instacartredshift',
                         host='redshift-cluster-1.cfxnwqcezz7k.us-east-1.redshift.amazonaws.com',
                         port= '5439',
                         user= 'root',
-                        password= 'Abcd1234')
-cur = db.cursor()
-currs = rsdb.cursor()
+                        password= 'Abcd1234')'''
+
+#currs = rsdb.cursor()
 result = ""
+
 @app.route('/',methods=['GET','POST'])
 def index():
     global result
+    result=""
     if request.method == 'POST':
-        query_con = request.form['query']
-        option = request.form['database']
-        if option=="mysql":
-            print(option)
-            tic = time()
-            cur.execute(query_con)
-            columns = cur.description
-            key_list = []
-            data_row = cur.fetchall()
-            for i in range(len(columns)):
-                key_list.append(columns[i][0])
-            df=DataFrame(data_row)
-            df.index = np.arange(1, len(df)+1)
-            df.columns=(key_list)
-            toc = time()
-            html_out = '<div style="background-color:white;display:inline-block;margin-left: auto;margin-right: auto;">'+df.to_html()+'</div>'
-            result = '<h3 style="color:white;">'+"Time elapsed(in seconds):"+str(round((toc-tic),4))+'</h3>'+'<br>'+html_out
+            try:
+                query_con = request.form['query']
+                option = request.form['database']
 
-        elif option=="redshift":
-            print(option)
-            tic = time()
-            currs.execute(query_con)
-            columns = currs.description
-            key_list = []
-            data_row = currs.fetchall()
-            for i in range(len(columns)):
-                key_list.append(columns[i][0])
-            df=DataFrame(data_row)
-            df.index = np.arange(1, len(df)+1)
-            df.columns=(key_list)
-            toc = time()
-            html_out = '<div style="background-color:white;display:inline-block;margin-left: auto;margin-right: auto;">'+df.to_html()+'</div>'
-            result = '<h3 style="color:white;">'+"Time elapsed(in seconds):"+str(round((toc-tic),4))+'</h3>'+'<br>'+html_out
+                if option=="mysql":
+                    db = pymysql.connect(host="database-2.cuxnpjy7qt1g.us-east-1.rds.amazonaws.com",
+                                         user="admin",
+                                         passwd="abcd1234",
+                                         db="instacart_db")
+                    cur = db.cursor()
+                    tic = time()
+                    r = cur.execute(query_con)
+                    if cur.description==() and r==0:
+                        html_out='<h3 style="color:white">Query Executed</h3>'
+                    else:
+                        columns = cur.description
+                        key_list = []
+                        data_row = cur.fetchall()
+                        for i in range(len(columns)):
+                            key_list.append(columns[i][0])
+                        df=DataFrame(data_row)
+                        if r==0:
+                            df=DataFrame(columns=key_list)
+                        else:
+                            df.index = np.arange(1, len(df)+1)
+                            df.columns=(key_list)
+                        html_out = '<div style="background-color:white;display:inline-block;margin-left: auto;margin-right: auto;">'+df.to_html(classes='table table-striped table-hover')+'</div>'
+                    cur.close()
+                    db.close()
+                    toc = time()
+                    result = '<h3 style="color:white;">'+"Time elapsed(in seconds):"+str(round((toc-tic),4))+'</h3>'+'<br>'+html_out
 
-        else:
-            result = "Please select a database name"
+                elif option=="redshift":
+                    rsdb = psycopg2.connect(dbname= 'instacartredshift',
+                                            host='redshift-cluster-1.cfxnwqcezz7k.us-east-1.redshift.amazonaws.com',
+                                            port= '5439',
+                                            user= 'root',
+                                            password= 'Abcd1234')
+                    currs = rsdb.cursor()
+                    print(option)
+                    tic = time()
+                    currs.execute(query_con)
+                    if "select" not in query_con and "show" not in query_con and "describe" not in query_con:
+                        rsdb.commit()
+                        html_out='<h3 style="color:white">Query Executed</h3>'
+                    else:
+                        columns = currs.description
+                        key_list = []
+                        data_row = currs.fetchall()
+                        for i in range(len(columns)):
+                            key_list.append(columns[i][0])
+                        df=DataFrame(data_row)
+                        df.index = np.arange(1, len(df)+1)
+                        df.columns=(key_list)
+                        html_out = '<div style="background-color:white;display:inline-block;margin-left: auto;margin-right: auto;">'+df.to_html(classes='table table-striped table-hover')+'</div>'
+                    currs.close()
+                    rsdb.close()
+                    toc = time()
+                    result = '<h3 style="color:white;">'+"Time elapsed(in seconds):"+str(round((toc-tic),4))+'</h3>'+'<br>'+html_out
+            except Exception as e:
+                result = '<h3 style="color:white;">'+str(e)+'</h3>'
+
+
 
     return render_template('index.html')+result
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0",port=8080)
