6dfb8d19ea193e5c121403bd457e60b66c95c7df
Dhaivat Patel <79534543+Dhaivat717@users.noreply.github.com>
2021-03-24 10:07:21 -0400

    Final version adds download csv file download capability

--- instacart1/app.py
+++ instacart1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request,Response
 import sqlalchemy
 import pymysql
 from pandas import DataFrame
@@ -6,29 +6,48 @@ from IPython.display import HTML
 from time import time
 import numpy as np
 import psycopg2
+from flask import send_file
+import pdfkit
+
 
 app = Flask(__name__)
 
-
-
-'''rsdb = psycopg2.connect(dbname= 'instacartredshift',
-                        host='redshift-cluster-1.cfxnwqcezz7k.us-east-1.redshift.amazonaws.com',
-                        port= '5439',
-                        user= 'root',
-                        password= 'Abcd1234')'''
-
-#currs = rsdb.cursor()
 result = ""
+csv_file = None
+pdf_file = None
+html_out = ""
+
+@app.route("/getPlotCSV")
+def getPlotCSV():
+    csv = '1,2,3\n4,5,6\n'
+    return Response(
+        csv_file,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=output.csv"})
+
+@app.route("/getPdf")
+def getPdf():
+    if html_out!="":
+        pdfkit.from_url('http://ec2-18-232-130-25.compute-1.amazonaws.com:8080/', pdf_file)
+    return Response(
+    pdf_file,
+    mimetype="text/pdf",
+    headers={"Content-disposition":
+             "attachment; filename=output.pdf"}
+    )
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    global result
+
+    global result,csv_file,html_out
+    csv_file = None
+    html_out = ""
     result=""
     if request.method == 'POST':
             try:
                 query_con = request.form['query']
                 option = request.form['database']
-
                 if option=="mysql":
                     db = pymysql.connect(host="database-2.cuxnpjy7qt1g.us-east-1.rds.amazonaws.com",
                                          user="admin",
@@ -43,6 +62,8 @@ def index():
                         for i in range(len(columns)):
                             key_list.append(columns[i][0])
                         df=DataFrame(columns=key_list)
+                        csv_file = df.to_csv()
+                        html_out = df.to_html()
                         c=1
                     elif cur.description is None and r==0:
                         c=2
@@ -60,9 +81,10 @@ def index():
                         else:
                             df.index = np.arange(1, len(df)+1)
                             df.columns=(key_list)
+                        csv_file = df.to_csv()
+                        html_out = df.to_html()
                         c=0
-                            
-                    
+
                     db.commit()
                     cur.close()
                     db.close()
@@ -72,7 +94,7 @@ def index():
                     if c==3:
                         return render_template('index.html',column_names=[],row_data=[],exec_time = ("Time Elapsed(in sec): "+str(round(toc-tic,7))),zip=zip)+"Query Executed: "+str(r)+" rows affected"
                     if c==2:
-                        return render_template('index.html',column_names=[],row_data=[],exec_time = ("Time Elapsed(in sec): "+str(round(toc-tic,7))),zip=zip)+"Query Executed: "    
+                        return render_template('index.html',column_names=[],row_data=[],exec_time = ("Time Elapsed(in sec): "+str(round(toc-tic,7))),zip=zip)+"Query Executed: "
                     if c==0:
                         return render_template('index.html',column_names=df.columns.values,row_data=list(df.values.tolist()),exec_time = ("Time Elapsed(in sec): "+str(round(toc-tic,7))),zip=zip)
 
@@ -100,15 +122,16 @@ def index():
                         else:
                             df.index = np.arange(1, len(df)+1)
                             df.columns=(key_list)
-                        c=0        
-                    
+                        csv_file=df.to_csv()
+                        c=0
+
                     db.commit()
                     cur.close()
                     db.close()
                     toc = time()
-        
+
                     if c==2:
-                        return render_template('index.html',column_names=[],row_data=[],exec_time = ("Time Elapsed(in sec): "+str(round(toc-tic,7))),zip=zip)+"Query Executed: "    
+                        return render_template('index.html',column_names=[],row_data=[],exec_time = ("Time Elapsed(in sec): "+str(round(toc-tic,7))),zip=zip)+"Query Executed: "
                     if c==0:
                         return render_template('index.html',column_names=df.columns.values,row_data=list(df.values.tolist()),exec_time = ("Time Elapsed(in sec): "+str(round(toc-tic,7))),zip=zip)
 
@@ -117,6 +140,9 @@ def index():
                 return render_template('index.html',column_names=[],row_Data=list(),exec_time="")+result
     else:
         return render_template('index.html',column_names=[],row_data=list(),exec_time="")
+
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080,debug=True)
